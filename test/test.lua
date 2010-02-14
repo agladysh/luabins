4416314a4ac587f5b0df57ca5a1244ea46663304
Alexander Gladysh <agladysh@gmail.com>
2010-02-15 02:41:32 +0300

    test.lua: enhanced logging

--- test/test.lua
+++ test/test.lua
@@ -188,6 +188,7 @@ local check_fail_save = function(msg, ...)
   local res, err = luabins.save(...)
   ensure_equals("result", res, nil)
   ensure_equals("error message", err, msg)
+  print("/check_fail_save")
 end
 
 local check_fail_load = function(msg, v)
@@ -195,6 +196,7 @@ local check_fail_load = function(msg, v)
   local res, err = luabins.load(v)
   ensure_equals("result", res, nil)
   ensure_equals("error message", err, msg)
+  print("/check_fail_load")
 end
 
 -- ----------------------------------------------------------------------------
