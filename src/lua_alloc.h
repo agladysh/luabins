809899053c537d418ce4bfeb1cf549e3429593c6
Alexander Gladysh <agladysh@gmail.com>
2010-02-16 03:18:46 +0300

    lua_alloc.h: oops, forgot include guards

--- src/lua_alloc.h
+++ src/lua_alloc.h
@@ -4,8 +4,13 @@
 * See copyright notice in luabins.h
 */
 
+#ifndef LUABINS_LUA_ALLOC_H_INCLUDED_
+#define LUABINS_LUA_ALLOC_H_INCLUDED_
+
 typedef void * (*lua_Alloc) (void *ud,
                              void *ptr,
                              size_t osize,
                              size_t nsize)
                              ;
+
+#endif /* LUABINS_LUA_ALLOC_H_INCLUDED_ */
