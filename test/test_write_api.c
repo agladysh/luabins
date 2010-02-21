d82761f4284f6bc00de9f85536defe3e8ff7c325
Alexander Gladysh <agladysh@gmail.com>
2010-02-22 00:36:04 +0300

    test/write_api: generalized write tests

--- test/test_write_api.c
+++ test/test_write_api.c
@@ -17,18 +17,9 @@
 #include "write.h"
 
 #include "test.h"
+#include "util.h"
 
 /******************************************************************************/
-
-static void fprintbuf(FILE * out, const unsigned char * b, size_t len)
-{
-  size_t i = 0;
-  for (i = 0; i < len; ++i)
-  {
-    fprintf(out, "%02X ", b[i]);
-  }
-  fprintf(out, "\n");
-}
 
 /*
 * Note it is different from test_savebuffer variant.
@@ -72,45 +63,27 @@ static void check_buffer(
 
 /******************************************************************************/
 
-TEST (test_writeTupleSize,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
+#define CAT(a, b) a ## b
 
-  {
-    unsigned char tuple_size = 0xAB;
+#define TEST_NAME(x) CAT(test_write, x)
+#define CALL_NAME(x) CAT(lbs_write, x)
+#define BUFFER_NAME (&sb)
+#define INIT_BUFFER \
+  luabins_SaveBuffer sb; \
+  lbsSB_init(BUFFER_NAME, lbs_simplealloc, NULL);
 
-    lbs_writeTupleSize(&sb, tuple_size);
-    check_buffer(&sb, "\xAB", 1);
-  }
+#define DESTROY_BUFFER \
+  lbsSB_destroy(BUFFER_NAME);
 
-  lbsSB_destroy(&sb);
-})
+#define CHECK_BUFFER check_buffer
 
-/******************************************************************************/
-
-TEST (test_writeTableHeader,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
-
-  {
-    int array_size = 0xAB;
-    int hash_size = 0xCD;
-
-    lbs_writeTableHeader(&sb, array_size, hash_size);
-    check_buffer(&sb, "T" "\xAB\x00\x00\x00" "\xCD\x00\x00\x00", 1 + 4 + 4);
-  }
-
-  lbsSB_destroy(&sb);
-})
+#include "write_tests.inc"
 
 /******************************************************************************/
 
 TEST (test_writeTableHeaderAt,
 {
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
+  INIT_BUFFER;
 
   {
     unsigned char tuple_size = 0x01;
@@ -118,11 +91,11 @@ TEST (test_writeTableHeaderAt,
     int hash_size = 0x00;
     int table_header_pos = 0;
 
-    lbs_writeTupleSize(&sb, tuple_size);
-    table_header_pos = lbsSB_length(&sb);
-    lbs_writeTableHeader(&sb, array_size, hash_size);
+    lbs_writeTupleSize(BUFFER_NAME, tuple_size);
+    table_header_pos = lbsSB_length(BUFFER_NAME);
+    lbs_writeTableHeader(BUFFER_NAME, array_size, hash_size);
 
-    check_buffer(
+    CHECK_BUFFER(
         &sb,
         "\x01" "T" "\x00\x00\x00\x00" "\x00\x00\x00\x00",
         1 + 1 + 4 + 4
@@ -131,149 +104,27 @@ TEST (test_writeTableHeaderAt,
     array_size = 0xAB;
     hash_size = 0xCD;
 
-    lbs_writeTableHeaderAt(&sb, table_header_pos, array_size, hash_size);
-    check_buffer(
-        &sb,
+    lbs_writeTableHeaderAt(
+        BUFFER_NAME,
+        table_header_pos,
+        array_size,
+        hash_size
+      );
+    CHECK_BUFFER(
+        BUFFER_NAME,
         "\x01" "T" "\xAB\x00\x00\x00" "\xCD\x00\x00\x00",
         1 + 1 + 4 + 4
       );
   }
 
-  lbsSB_destroy(&sb);
-})
-
-/******************************************************************************/
-
-TEST (test_writeNil,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
-
-  {
-    lbs_writeNil(&sb);
-    check_buffer(&sb, "-", 1);
-  }
-
-  lbsSB_destroy(&sb);
-})
-
-/******************************************************************************/
-
-TEST (test_writeBoolean,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
-
-  {
-    lbs_writeBoolean(&sb, 1);
-    check_buffer(&sb, "1", 1);
-
-    lbs_writeBoolean(&sb, 0);
-    check_buffer(&sb, "10", 1 + 1);
-  }
-
-  lbsSB_destroy(&sb);
-})
-
-/******************************************************************************/
-
-TEST (test_writeNumber,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
-
-  {
-    /* Note number is a double */
-    lbs_writeNumber(&sb, 1.0);
-    check_buffer(&sb, "N" "\x00\x00\x00\x00\x00\x00\xF0\x3F", 1 + 8);
-  }
-
-  lbsSB_destroy(&sb);
-})
-
-/******************************************************************************/
-
-TEST (test_writeInteger,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
-
-  {
-    /* Note integer is alsow written as a double */
-    lbs_writeInteger(&sb, 1);
-    check_buffer(&sb, "N" "\x00\x00\x00\x00\x00\x00\xF0\x3F", 1 + 8);
-  }
-
-  lbsSB_destroy(&sb);
-})
-
-/******************************************************************************/
-
-TEST (test_writeStringEmpty,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
-
-  {
-    lbs_writeString(&sb, "", 0);
-    check_buffer(&sb, "S" "\x00\x00\x00\x00\x00\x00\x00\x00", 1 + 8);
-  }
-
-  lbsSB_destroy(&sb);
-})
-
-TEST (test_writeStringSimple,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
-
-  {
-    lbs_writeString(&sb, "Luabins", 7);
-    check_buffer(
-        &sb,
-        "S" "\x07\x00\x00\x00\x00\x00\x00\x00" "Luabins",
-        1 + 8 + 7
-      );
-  }
-
-  lbsSB_destroy(&sb);
-})
-
-TEST (test_writeStringEmbeddedZero,
-{
-  luabins_SaveBuffer sb;
-  lbsSB_init(&sb, lbs_simplealloc, NULL);
-
-  {
-    lbs_writeString(&sb, "Embedded\0Zero", 13);
-    check_buffer(
-        &sb,
-        "S" "\x0D\x00\x00\x00\x00\x00\x00\x00" "Embedded\0Zero",
-        1 + 8 + 13
-      );
-  }
-
-  lbsSB_destroy(&sb);
+  DESTROY_BUFFER;
 })
 
 /******************************************************************************/
 
 void test_write_api()
 {
-  test_writeTupleSize();
+  RUN_GENERATED_TESTS;
 
-  test_writeTableHeader();
   test_writeTableHeaderAt();
-
-  test_writeNil();
-
-  test_writeBoolean();
-
-  test_writeNumber();
-
-  test_writeInteger();
-
-  test_writeStringEmpty();
-  test_writeStringSimple();
-  test_writeStringEmbeddedZero();
 }
